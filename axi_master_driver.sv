6298982ebd0b8b6a34f90d9ffb6be155cb27df18
程景峰 <496272507@qq.com>
2017-06-04 15:17:42 +0800

    Update axi_master_driver.sv

--- axi_master_driver.sv
+++ axi_master_driver.sv
@@ -10,50 +10,25 @@
 class axi_master_driver extends uvm_driver #(axi_transaction);
 
   virtual interface axi_if    m_vif;
-  axi_master_conf             m_confg;
-  axi_transaction                m_wr_queue[$];
-  axi_transaction                m_rd_queue[$];
-  int unsigned                m_num_sent;
-
-  event                       event_sent_write_trx;
-  event                       event_sent_read_trx;
-
+  axi_transaction             m_wr_queue[$];
+  axi_transaction             m_rd_queue[$];
   int unsigned                m_wr_addr_indx = 0;
   int unsigned                m_wr_data_indx = 0;
   int unsigned                m_rd_addr_indx = 0;
-
-	`uvm_component_utils_begin(axi_master_driver)
-		`uvm_field_int	(m_num_sent,UVM_ALL_ON)
-	`uvm_component_utils_end
 
   function new (string name, uvm_component parent);
     super.new(name, parent);
   endfunction:new
 
-  // Additional class methods
-  //extern virtual function void assign_vif(virtual interface axi_if vif);
-  //extern virtual function void assign_conf(axi_master_conf conf);
-  
   extern virtual task run_phase(uvm_phase phase);
   extern virtual function void connect_phase(uvm_phase phase);
-  extern virtual protected task get_and_drive();
   extern virtual protected task reset();
-  extern virtual protected task drive_transfer(AXI_transfer trx);
- // extern virtual function void report();
-
-  extern virtual task sent_addr_write_trx();
-  extern virtual task sent_data_write_trx();
-  extern virtual task received_resp_write_trx();
-
-  extern virtual task sent_addr_read_trx();
-  extern virtual task received_data_read_trx();
-
-  extern virtual task free_write_trx();
-  extern virtual task free_read_trx();
-
-  extern virtual protected task wait_for_reset();
-  extern virtual protected task sent_trx_to_seq();
-
+  
+  extern virtual task write_addr();
+  extern virtual task write_data();
+  extern virtual task received_resp_write();
+  extern virtual task read_addr();
+  extern virtual task read_data();
 endclass : axi_master_driver
 
 //UVM connect_phase
@@ -70,12 +45,15 @@ task axi_master_driver::run_phase(uvm_phase phase);
     forever begin
      seq_item_port.get_next_item(seq);
      case(seq.op_cmd)
-     	RESET:begin
-		reset();end
-	WRITE:begin 
-		write_addr();write_data;end
-	READ:begin
-		read_addr();read_data;end
+     	RESET:fork
+		reset();
+	      join
+	WRITE:fork 
+		write_addr();write_data();received_resp_write();
+	      join
+	READ: fork
+		read_addr();read_data;
+	      join
 	default:`uvm_fatal("axi_driver","No valid command")
      endcase
      seq_item_port.item_done();
@@ -99,13 +77,13 @@ task axi_master_driver::reset();
 endtask:reset
 
 //TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT
-//task:write
+//task:write_addr
 //description:axi write address channel
 //TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT
 task axi_master_driver::write_addr();
     axi_transaction m_tr;
     forever begin
-      // if write trx has existed...
+      // if write tr has existed...
       repeat(m_wr_queue.size()==0) @(posedge m_vif.ACLK);
 
       if (m_wr_addr_indx < m_wr_queue.size()) begin
@@ -113,30 +91,29 @@ task axi_master_driver::write_addr();
           repeat(m_trx.addr_wt_delay) @(posedge m_vif.ACLK);
 	  
           // sent tr
-          m_vif.AXI_AWVALID <= 1'b1;
-          m_vif.AXI_AWID    <= m_tr.id;
-          m_vif.AXI_AWADDR  <= m_tr.addr;
-          m_vif.AXI_AWREG   <= m_tr.region;
-          m_vif.AXI_AWLEN   <= m_tr.len;
-          m_vif.AXI_AWSIZE  <= m_tr.size;
-          m_vif.AXI_AWBURST <= m_tr.burst;
-          m_vif.AXI_AWLOCK  <= m_tr.lock;
-          m_vif.AXI_AWCACHE <= m_tr.cache;
-          m_vif.AXI_AWPROT  <= m_tr.prot;
-          m_vif.AXI_AWQOS   <= m_tr.qos;
+          m_vif.AWVALID <= 1'b1;
+          m_vif.AWID    <= m_tr.id;
+          m_vif.AWADDR  <= m_tr.addr;
+          m_vif.AWREG   <= m_tr.region;
+          m_vif.AWLEN   <= m_tr.len;
+          m_vif.AWSIZE  <= m_tr.size;
+          m_vif.AWBURST <= m_tr.burst;
+          m_vif.AWLOCK  <= m_tr.lock;
+          m_vif.AWCACHE <= m_tr.cache;
+          m_vif.AWPROT  <= m_tr.prot;
+          m_vif.AWQOS   <= m_tr.qos;
           @(posedge m_vif.ACLK);
 
           //wait AWREADY
-          while (!m_vif.AXI_AWREADY) @(posedge m_vif.ACLK);
+          while (!m_vif.AWREADY) @(posedge m_vif.ACLK);
 	  @(posedge m_vif.ACLK);
-          m_vif.AXI_AWVALID <= 1'b0;
+          m_vif.AWVALID <= 1'b0;
           m_wr_addr_indx += 1;
       end 
       else begin
         @(posedge m_vif.ACLK);
       end
     end
-
 endtask:write
 
 //TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT
@@ -146,7 +123,6 @@ endtask:write
 task axi_master_driver::write_data();
     int unsigned  i = 0;
     axi_transaction  m_tr;
-
     forever begin
       repeat(m_wr_queue.size()==0) @(posedge m_vif.ACLK);
       if (m_wr_data_indx < m_wr_queue.size()) begin
@@ -179,41 +155,37 @@ task axi_master_driver::write_data();
       end
 endtask:write_data
 
-
-// data resp trx collect resp to trx
-task axi_master_driver::received_resp_write_trx();
-
+//TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT
+//task:received_resp_write
+//description:axi write respone channel
+//TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT
+task axi_master_driver::received_resp_write();
   forever begin
-    `delay(m_conf.half_cycle);
-     m_vif.AXI_BREADY <= 1'b0;
-     repeat($urandom_range(4,8)) @(posedge m_vif.ACLK);
+     m_vif.BREADY <= 1'b0;
+     repeat(2) @(posedge m_vif.ACLK);
 
-    `delay(m_conf.half_cycle);
-     m_vif.AXI_BREADY <= 1'b1;
+     m_vif.BREADY <= 1'b1;
      @(posedge m_vif.ACLK);
 
-    // hold until BVALID received
+    //wait BVALID received
     while(!m_vif.BVALID) @(posedge m_vif.ACLK);
   end
+endtask : received_resp_write
 
-endtask : received_resp_write_trx
-
-
-// addr read trx
-task axi_master_driver::sent_addr_read_trx();
-    AXI_transfer m_trx;
+//TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT
+//task:read_addr
+//description:axi read address channel
+//TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT
+task axi_master_driver::read_addr();
+    axi_transaction m_tr;
 
     forever begin
-
       repeat(m_rd_queue.size()==0) @(posedge m_vif.ACLK);
-
       if (m_rd_addr_indx < m_rd_queue.size()) begin
           m_trx = m_rd_queue[m_rd_addr_indx];
-
           repeat(m_tr.addr_rd_delay) @(posedge m_vif.ACLK);
 
           // sent tr
-          `delay(m_conf.half_cycle);
           m_vif.ARVALID <= 1'b1;
           m_vif.ARID    <= m_tr.id;
           m_vif.ARADDR  <= m_tr.addr;
@@ -227,50 +199,38 @@ task axi_master_driver::sent_addr_read_trx();
           m_vif.ARQOS   <= m_tr.qos;
           @(posedge m_vif.ACLK);
 
-          // hold until ARREADY received
+          //wait ARREADY received
           while(!m_vif.ARREADY) @(posedge m_vif.ACLK);
-          //void'(m_rd_queue.pop_front());
-
-          // free trx
-         `delay(m_conf.half_cycle);
-         m_vif.ARVALID <= 1'b0;
-         @(posedge m_vif.ACLK);
-
-         m_rd_addr_indx += 1;
-
-      end else begin
+    	  @(posedge m_vif.ACLK);
+          m_vif.ARVALID <= 1'b0;
+          @(posedge m_vif.ACLK);
+	  m_rd_addr_indx += 1;
+      end 
+      else begin
         @(posedge m_vif.ACLK);
       end
     end
+endtask : read_addr
 
-endtask : sent_addr_read_trx
-
-
-// data read trx
-task axi_master_driver::received_data_read_trx();
-
+//TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT
+//task:read_data
+//description:axi read data channel
+//TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT
+task axi_master_driver::read_data(output [AXI_DATA_WIDTH-1:0] rd_data);
+  always@(posedge m_vif.ACLK)
+  rd_data = (m_vif.RVALID && m_vif.RREADY)? m_vif.RDATA:0;
   forever begin
-    `delay(m_conf.half_cycle);
-     m_vif.AXI_RREADY <= 1'b0;
-     repeat($urandom_range(4,8)) @(posedge m_vif.AXI_ACLK);
-
-    `delay(m_conf.half_cycle);
-     m_vif.AXI_RREADY <= 1'b1;
-    @(posedge m_vif.AXI_ACLK);
-
-     // hold until RVALID received
-     while(!m_vif.AXI_RVALID) @(posedge m_vif.AXI_ACLK);
-
-    // continuous burst case
-    `delay(m_conf.half_cycle);
-    m_vif.AXI_RREADY <= 1'b1;
-    repeat($urandom_range(4,16)) @(posedge m_vif.AXI_ACLK);
+     m_vif.RREADY <= 1'b0;
+     repeat(2) @(posedge m_vif.ACLK);
+     m_vif.RREADY <= 1'b1;
+     @(posedge m_vif.ACLK);
+     
+     //wait RVALID received
+     while(!m_vif.RVALID) @(posedge m_vif.ACLK);   
+     // continuous burst case
+     while(!m_vif.RLAST) @(posedge m_vif.ACLK);
+     m_vif.RREADY <= 1'b0;
   end
-
-endtask : received_data_read_trx
-
-
-// finish read trx
+endtask : read_data
 
 `endif // AXI_MASTER_DRIVER__SV
-
